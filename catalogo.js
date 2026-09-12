ca0477df44c9af3a68022f0fe9d1a7762e7dacae
Rodrigo, Salvador Martinez <rodrigosalvador93@gmail.com>
2026-09-12 11:37:48 -0300

    Actualizacion automatica - 12/9/2026, 11:36:44

--- catalogo.js
+++ catalogo.js
@@ -1,5 +1,5 @@
-// JR Shop — generado 2026-09-12 14:15
-window.JR_VERSION = "2026-09-12 14:15";
+// JR Shop — generado 2026-09-12 14:36
+window.JR_VERSION = "2026-09-12 14:36";
 
 
 const CART_PHONE = "543812235528";
